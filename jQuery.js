df017c8e4b6de0a12e5870198c9e5fcec6806cea
bramw <bramwicherink1999@gmail.com>
2019-01-04 10:53:55 +0100

    Removed a piece of code into jspdf.debug.js to make sure that you do not have to add the five maximum required fields. I also made the fields a little bit more compact (code)

--- jQuery.js
+++ jQuery.js
@@ -1,23 +1,126 @@
 $(document).ready(function () {
-    var max_fields = 5; // Maximaal aantal toegestane velden
-    var wrapper = $(".input_fields_wrap");
-    var add_button = $(".add_field_button");
+    let max_fields = 5; // Maximaal aantal toegestane velden
+    let wrapper = $(".input_fields_wrap");
+    let add_button = $(".add_field_button");
 
 
-    var x = 1; //initlal text box count
-    var extra_id = 1;
+    let x = 1; //initlal text box count
+    let extra_id = 1;
+    let doc = new jsPDF();
+    doc.addImage(imgData, 'JPEG',0,0,210,297);
+    doc.setTextColor(0.7544, 0.4035, 0.0000, 0.5529);
+    let voornaam = $('#voornaam').val();
+    let achternaam = $('#achternaam').val();
+    let adresgegevens = $('#adresgegevens').val();
+    let telefoon = $('#telefoon').val();
+    let email = $('#email').val();
+    let geboortedatum = $('#geboortedatum').val();
+    let geboorteplaats = $('#geboorteplaats').val();
+    let nationaliteit = $('#nationaliteit').val();
+    let naamOpleiding = $('#naam_opleiding').val();
+    let naamInstituut = $('#naam_instituut').val();
+    let startdatum = $('#startdatum').val();
+    let einddatum = $('#einddatum').val();
+    let overigeInformatie = $('#overige_informatie').val();
+
+
+    let naamOpleiding2 = $('#naam_opleiding2').val();
+    let naamInstituut2 = $('#naam_instituut2').val();
+    let startdatum2 = $('#startdatum2').val();
+    let einddatum2 = $('#einddatum2').val();
+    let overigeInformatie2 = $('#overige_informatie2').val();
+
+
+    let naamOpleiding3 = $('#naam_opleiding3').val();
+    let naamInstituut3 = $('#naam_instituut3').val();
+    let startdatum3 = $('#startdatum3').val();
+    let einddatum3 = $('#einddatum3').val();
+    let overigeInformatie3 = $('#overige_informatie3').val();
+
+
+    let naamOpleiding4 = $('#naam_opleiding4').val();
+    let naamInstituut4 = $('#naam_instituut4').val();
+    let startdatum4 = $('#startdatum4').val();
+    let einddatum4 = $('#einddatum4').val();
+    let overigeInformatie4 = $('#overige_informatie4').val();
+
+
+    let naamOpleiding5 = $('#naam_opleiding5').val();
+    let naamInstituut5 = $('#naam_instituut5').val();
+    let startdatum5 = $('#startdatum5').val();
+    let einddatum5 = $('#einddatum5').val();
+    let overigeInformatie5 = $('#overige_informatie5').val();
+
     $(add_button).click(function (e) { //on add input button click
         e.preventDefault();
         extra_id++;
         alert(extra_id);
 
 
-        /* if ($(add_button).click) {
-             for (extra_id = 0; extra_id < max_fields; extra_id++) {
-                 $("#startdatum").attr("id", "startdatum" + extra_id);
-             }
-         } */
 
+
+
+        $('.button-cv-maken').click(function() {
+
+
+            doc.text(40, 27, voornaam);
+            doc.setFontSize(10);
+            doc.text(40, 32, achternaam);
+            doc.text(40, 37, adresgegevens);
+            doc.text(40, 42, telefoon);
+            doc.text(40, 47, email);
+            doc.text(40, 52, geboortedatum);
+            doc.text(40, 57, geboorteplaats);
+            doc.text(40, 67, nationaliteit);
+
+            doc.setFontSize(25);
+            doc.text(110, 27, naamOpleiding);
+            doc.setFontSize(10);
+            doc.text(110, 32, naamInstituut);
+            doc.text(110, 37, startdatum);
+            doc.text(110, 42, einddatum);
+            doc.text(110, 47, overigeInformatie);
+
+            doc.setFontSize(25);
+            doc.text(110, 60, naamOpleiding2);
+            doc.setFontSize(10);
+            doc.text(110, 65, naamInstituut2);
+            doc.text(110, 70, startdatum2);
+            doc.text(110, 75, einddatum2);
+            doc.text(110, 80, overigeInformatie2);
+
+            doc.setFontSize(25);
+            doc.text(110, 93, naamOpleiding3);
+            doc.setFontSize(10);
+            doc.text(110, 98, naamInstituut3);
+            doc.text(110, 103, startdatum3);
+            doc.text(110, 108, einddatum3);
+            doc.text(110, 113, overigeInformatie3);
+
+            doc.setFontSize(25);
+            doc.text(110, 126, naamOpleiding4);
+            doc.setFontSize(10);
+            doc.text(110, 131, naamInstituut4);
+            doc.text(110, 136, startdatum4);
+            doc.text(110, 141, einddatum4);
+            doc.text(110, 146, overigeInformatie4);
+
+            doc.setFontSize(25);
+            doc.text(110, 159, naamOpleiding5);
+            doc.setFontSize(10);
+            doc.text(110, 164, naamInstituut5);
+            doc.text(110, 169, startdatum5);
+            doc.text(110, 174, einddatum5);
+            doc.text(110, 179, overigeInformatie5);
+            doc.save('CV ' + voornaam + '' + achternaam + '.pdf');
+
+
+            if (x < max_fields) {
+
+                doc.save('CV ' + voornaam + '' + achternaam + '.pdf');
+            }
+
+        });
 
 
 
@@ -39,6 +142,7 @@ $(document).ready(function () {
 
 
         }
+
 
 
         $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
