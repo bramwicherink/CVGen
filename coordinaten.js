c0ca23535986fa0b70db4e7813b4db32b7647e6c
bramw <bramwicherink1999@gmail.com>
2019-01-04 10:03:55 +0100

    Trying with console.log where it goes wrong

---
+++ coordinaten.js
@@ -0,0 +1,49 @@
+doc.text(40, 27, voornaam);
+doc.setFontSize(10);
+doc.text(40, 32, achternaam);
+doc.text(40, 37, adresgegevens);
+doc.text(40, 42, telefoon);
+doc.text(40, 47, email);
+doc.text(40, 52, geboortedatum);
+doc.text(40, 57, geboorteplaats);
+doc.text(40, 67, nationaliteit);
+
+doc.setFontSize(25);
+doc.text(110, 27, naamOpleiding);
+doc.setFontSize(10);
+doc.text(110, 32, naamInstituut);
+doc.text(110, 37, startdatum);
+doc.text(110, 42, einddatum);
+doc.text(110, 47, overigeInformatie);
+
+doc.setFontSize(25);
+doc.text(110, 60, naamOpleiding2);
+doc.setFontSize(10);
+doc.text(110, 65, naamInstituut2);
+doc.text(110, 70, startdatum2);
+doc.text(110, 75, einddatum2);
+doc.text(110, 80, overigeInformatie2);
+
+doc.setFontSize(25);
+doc.text(110, 93, naamOpleiding3);
+doc.setFontSize(10);
+doc.text(110, 98, naamInstituut3);
+doc.text(110, 103, startdatum3);
+doc.text(110, 108, einddatum3);
+doc.text(110, 113, overigeInformatie3);
+
+doc.setFontSize(25);
+doc.text(110, 126, naamOpleiding4);
+doc.setFontSize(10);
+doc.text(110, 131, naamInstituut4);
+doc.text(110, 136, startdatum4);
+doc.text(110, 141, einddatum4);
+doc.text(110, 146, overigeInformatie4);
+
+doc.setFontSize(25);
+doc.text(110, 159, naamOpleiding5);
+doc.setFontSize(10);
+doc.text(110, 164, naamInstituut5);
+doc.text(110, 169, startdatum5);
+doc.text(110, 174, einddatum5);
+doc.text(110, 179, overigeInformatie5);
